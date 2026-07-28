5af04ab714250a509fda4b3de815ef17ef8f7e25
MohitJharbade <mohitjharbade999@gmail.com>
2026-07-29 02:12:33 +0530

    beckend  index const express has been changed

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const rateLimit = require('express-rate-limit');
 const preprocessImage = require('./services/imagePreprocess');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5001;
 
 app.use(helmet());
