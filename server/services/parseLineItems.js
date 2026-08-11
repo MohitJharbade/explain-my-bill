d952b92d643e749405270b1c960dc2361199f7bd
MohitJharbade <mohitjharbade999@gmail.com>
2026-08-12 02:39:37 +0530

    Style frontend: paper/ledger design with

--- server/services/parseLineItems.js
+++ server/services/parseLineItems.js
@@ -2,25 +2,29 @@ function parseLineItems(rawText) {
   const lines = rawText.split("\n").map((line) => line.trim()).filter(Boolean);
 
   const numberedRowPattern = /^\d+\s*[|.]?\s+(.+?)\s+\d+\s+[\d,]+\.\d{2}\s+([\d,]+\.\d{2})$/;
-  const simplePattern = /^(.+?)\s+\$?([\d,]+(?:\.\d{1,2})?)$/;
+  const simplePattern = /^(.+?)\s+\$?([\d,]+\.\d{2})$/;
 
   // Lines that look like a charge but actually aren't (tax, totals, summaries)
   const excludeTerms = [
-    "cgst",
-    "sgst",
-    "gst",
-    "sub total",
-    "subtotal",
-    "total amount",
-    "taxable amount",
-    "discount",
-    "amount in words",
-    "please keep",
-    "medicines once sold",
-    "in case of any queries",
-    "thank you for choosing",
-    "speedy recovery",
-  ];
+     "cgst",
+     "sgst",
+     "gst",
+     "sub total",
+     "subtotal",
+     "total amount",
+     "taxable amount",
+     "discount",
+     "amount in words",
+     "please keep",
+     "medicines once sold",
+     "in case of any queries",
+     "thank you for choosing",
+     "speedy recovery",
+     "pradesh",
+     "nagar",
+     "colony",
+     "apartment",
+   ];
 
   function isExcluded(description) {
     const lower = description.toLowerCase();
